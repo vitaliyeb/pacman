224e09e4067c9baa713e56717b8c1cbb1f281f7c
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-08-19 12:53:54 +0500

    написал функцию отрисовки углов

--- dev/js/game.js
+++ dev/js/game.js
@@ -44,25 +44,6 @@ import CreateMap from './map';
         }
     }
 
-    function trainingRenderId3 (){
-        //угол
-        let cx = xSteep/2;
-        let cy = ySteep/2;
-        let divisionSizeY = cy/4; 
-        let divisionSizeX = cx/4;
-
-        return (x, y)=>{
-            context.strokeStyle = '#1b1bcd';
-            context.lineWidth = 2;
-            context.moveTo(x+cx-divisionSizeX, y);
-            context.quadraticCurveTo(x+cx-divisionSizeX, y+cy+divisionSizeY, x+xSteep, y+cy+divisionSizeY);
-            context.moveTo(x+cx+divisionSizeX, y);
-            context.quadraticCurveTo(x+cx+divisionSizeX, y+cy-divisionSizeY, x+xSteep, y+cy-divisionSizeY);
-            context.stroke();
-            context.lineWidth = 1;
-        }
-    }
-
     function trainingRenderId4 (){
         //Вертикальная линия вокруг карты
         let cx = xSteep/2;
@@ -80,70 +61,37 @@ import CreateMap from './map';
         }
     }
 
-    function trainingRenderId5 (){
-        //угол
-        let cx = xSteep/2;
-        let cy = ySteep/2;
-        let divisionSizeY = cy/4; 
-        let divisionSizeX = cx/4;
-
-        return (x, y)=>{
-            context.strokeStyle = '#1b1bcd';
-            context.lineWidth = 2;
-            context.moveTo(x+cx-divisionSizeX, y);
-            context.quadraticCurveTo(x+cx-divisionSizeX, y+cy-divisionSizeY, x, y+cy-divisionSizeY);
-            context.moveTo(x+cx+divisionSizeX, y);
-            context.quadraticCurveTo(x+cx+divisionSizeX, y+cy+divisionSizeY, x, y+cy+divisionSizeY);
-            context.stroke();
-            context.lineWidth = 1;
-        }
-    }
-    function trainingRenderId6 (){
-        //угол
-        let cx = xSteep/2;
-        let cy = ySteep/2;
-        let divisionSizeY = cy/4; 
-        let divisionSizeX = cx/4;
-
-        return (x, y)=>{
-            context.strokeStyle = '#1b1bcd';
-            context.lineWidth = 2;
-            context.moveTo(x+cx-divisionSizeX, y);
-            context.quadraticCurveTo(x+cx-divisionSizeX, y+cy-divisionSizeY, x, y+cy-divisionSizeY);
-            // context.moveTo(x+cx+divisionSizeX, y);
-            // context.quadraticCurveTo(x+cx+divisionSizeX, y+cy+divisionSizeY, x, y+cy+divisionSizeY);
-            context.stroke();
-            context.lineWidth = 1;
-        }
-    }
-
     function trainingDubleAngle (){
         //угол
         let cx = xSteep/2;
         let cy = ySteep/2;
         let dy = cy/4; 
         let dx = cx/4;
+        function dotHelper (x, y, c){
+            context.fillStyle = c;
+            context.fillRect(x, y, 5, 5);
+            context.fill();
+        }
         return (x, y, r, c) => {
             let params = [];
 
             context.strokeStyle = '#1b1bcd';
             context.lineWidth = 2;
 
-            if(r != 0 && (gameMap[r-1][c] === 2 || gameMap[r-1][c] === 4)) params.push([x+cx, y]);
             if(r+1 != gameMap.length && (gameMap[r+1][c] === 2 || gameMap[r+1][c] === 4)) params.push([x+cx, y+ySteep]);
-            if(c != 0 && (gameMap[r][c-1] === 2 || gameMap[r][c-1] === 4)) params.push([x, y+cy]);
+            if(r != 0 && (gameMap[r-1][c] === 2 || gameMap[r-1][c] === 4)) params.push([x+cx, y]);
             if(c+1 != gameMap[r].length && (gameMap[r][c+1] === 2 || gameMap[r][c+1] === 4)) params.push([x+xSteep, y+cy]);
+            if(c != 0 && (gameMap[r][c-1] === 2 || gameMap[r][c-1] === 4)) params.push([x, y+cy]);
 
-            let s = params[0], e = params[1];
+            let [s, e] = params;
             let sx = s[0], sy = s[1], ex = e[0], ey = e[1];
-            for (let i = 0; i < 2; i++) {
-                
-                context.moveTo(x+cx-divisionSizeX, y);
-                context.quadraticCurveTo(x+cx-divisionSizeX, y+cy-divisionSizeY, x, y+cy-divisionSizeY);
-            }
-
-
-            context.stroke();
+            let my = sx < ex ? ey > sy ? -1 : 1 : ey > sy ? 1 : -1;
+            let mqx = sx < ex ? -1 : 1;
+            [-1, 1].map((k)=>{
+                context.moveTo(sx + (dx*k), sy);
+                context.quadraticCurveTo(ex + (cx * mqx) + (dx * k), ey + (dy * k * my), ex, ey + (dy * k * my));
+            });
+            context.stroke()
             context.lineWidth = 1;
         }
     }
@@ -152,10 +100,7 @@ import CreateMap from './map';
     function renderMap (){
         let renderId1 = trainingRenderId1();
         let renderId2 = trainingRenderId2();
-        let renderId3 = trainingRenderId3();
         let renderId4 = trainingRenderId4();
-        let renderId5 = trainingRenderId5();
-        let renderId6 = trainingRenderId6();
         let renderDubleAngle = trainingDubleAngle();
         
         gameMap.map((elRow, ir)=>{
@@ -175,13 +120,9 @@ import CreateMap from './map';
                     case 2:
                         return renderId2(x, y)
                     case 3:
-                        return renderId3(x, y)
+                        return renderDubleAngle(x, y, ir, ic) 
                     case 4:
-                        return renderId4(x, y)
-                    case 5:
-                        return renderId5(x, y)
-                    case 6:
-                        return renderDubleAngle(x, y, ir, ic)    
+                        return renderId4(x, y)         
                 }
 
 
