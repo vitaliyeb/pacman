16b2e2f2bc927a2e06ad35afe80924e6c8663b0d
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-08-25 12:07:29 +0500

    написал нормальное перемещение пакмена

--- dev/js/pacman.js
+++ dev/js/pacman.js
@@ -17,20 +17,22 @@ export default class Pacman {
         this.direction = [1, 0]; // x y
         this.anglePacman = 0;
         this.openMounth = true;
+        this.endChangeDirection = undefined;
         this.color = '#ffff00';
         this.name = 'pac man';
         this.r = xs < ys ? Math.floor(xs / 2) - 1 : Math.floor(ys / 2) - 1;
         this.createCounterMounth();
         this.keyEventId = window.addEventListener('keydown', ({code})=>{
+            let { direction: [xd , yd], setPropertyOnClick } = this;
             switch(code){
                 case 'ArrowUp':
-                    return this.changeCourse = [0, -1];
+                    return yd !=0 ? setPropertyOnClick(1, 0, -1) : setPropertyOnClick(0, 0, -1);                  
                 case 'ArrowLeft':
-                    return this.changeCourse = [-1, 0];
+                    return xd !=0 ? setPropertyOnClick(1, -1, 0) : setPropertyOnClick(0, -1, 0);
                 case 'ArrowRight':
-                    return this.changeCourse = [1, 0];
+                    return xd !=0 ? setPropertyOnClick(1, 1, 0) : setPropertyOnClick(0, 1, 0);
                 case 'ArrowDown':
-                    return this.changeCourse = [0, 1];            
+                    return yd !=0 ? setPropertyOnClick(1, 0, 1) : setPropertyOnClick(0, 0, 1);         
             }
         });
     }
@@ -48,9 +50,21 @@ export default class Pacman {
         
     }
 
+    setPropertyOnClick = (isDirection, x, y) => {
+        let { nextMapCoord:[ir , ic], direction: [dx , dy], mapCoord: [r, c], endChangeDirection } = this;
+        if(isDirection){
+            if(endChangeDirection === x || endChangeDirection === y) return;
+            this.direction = [x , y];
+            this.nextMapCoord = [ir + y , ic + x]
+            this.mapCoord = [ir , ic];
+            this.endChangeDirection = x === 0 ? y : x;
+        } else {
+            this.changeCourse = [x , y];
+        }
+    }
+
     updateCoords(c, tc, shift, v){
         let { nextMapCoord:[ir , ic], direction: [dx , dy], } = this;
-
         if(this[v] !== tc) return this[v] += shift;
 
         if(!this.changeCourse) this.nextMapCoord = [ir + dy , ic + dx];
@@ -58,20 +72,19 @@ export default class Pacman {
         if(this.changeCourse) {
             let { changeCourse:[nx, ny] } = this;
             this.nextMapCoord = [ir + ny , ic + nx];
+            this.direction = this.changeCourse;
+            this.changeCourse = undefined;
         }
 
         this.mapCoord = [ir , ic];
-           
-                    
-        
-        
+        this.endChangeDirection = undefined;
     }
 
     move() {
         let {x, y, direction: [ dx, dy], speed, xs, ys, mapCoord: [ir, ic], nextMapCoord: [nir, nic]} = this;
 
         if(ic !== nic) this.updateCoords(x , xs * nic, speed * dx, 'x');
-        if(ir !== nir) this.updateCoords(x , ys * nir, speed * dy, 'y');
+        if(ir !== nir) this.updateCoords(y , ys * nir, speed * dy, 'y');
 
     }
 
