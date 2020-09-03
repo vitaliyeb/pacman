e54bd04d94709097633c278ea1d526146a05eabb
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-09-03 11:25:07 +0500

    переход через стены без багов

--- dev/js/pacman.js
+++ dev/js/pacman.js
@@ -6,22 +6,19 @@ export default class Pacman {
         this.counterMounth = undefined;
         this.gameMap = gameMap;
         this.contextMap = contextMap;
-        this.maxX = gameMap[0].length * xs;
+        this.maxX = (gameMap[0].length-1) * xs;
         this.ys = ys;
         this.x = x;
         this.y = y;
         this.nextMapCoord = [ir , ic + 1]
         this.mapCoord = [ir, ic];
-        this.speed = 0.5;
+        this.speed = 1;
         this.angleOpenMounth = (Math.PI / 50 * (xs / 4 * 3)) / 2;
         this.changeCourse = undefined;
         this.aa = Math.PI / 2;
         this.direction = [1, 0]; // x y
         this.anglePacman = 0;
-        this.throughData = {
-            xCol: undefined,
-            xD: undefined
-        };
+        this.throughData = undefined;
         this.openMounth = true;
         this.stope = false;
         this.endChangeDirection = undefined;
@@ -31,7 +28,8 @@ export default class Pacman {
         this.r = xs < ys ? Math.floor(xs / 2) - 1 : Math.floor(ys / 2) - 1;
         this.createCounterMounth();
         this.keyEventId = window.addEventListener('keydown', ({code})=>{
-            let { direction: [xd , yd], setPropertyOnClick } = this;
+            let { direction: [xd , yd], setPropertyOnClick, isThrough } = this;
+            if ( isThrough ) return;
             switch(code){
                 case 'ArrowUp':
                     return yd !=0 ? setPropertyOnClick(1, 0, -1) : setPropertyOnClick(0, 0, -1);                  
@@ -53,8 +51,9 @@ export default class Pacman {
     }
 
     coolision(v, shift) {
-        let { stope, mapCoord:[mapCoordY, mapCoordX] ,gameMap, c, nextMapCoord: [nextMapY, nextMapX], changeCourse } = this;
-
+        let { stope, mapCoord:[mapCoordY, mapCoordX] ,gameMap, c, nextMapCoord: [nextMapY, nextMapX], changeCourse, isThrough } = this;
+        // console.log(gameMap[mapCoordY][mapCoordX])
+        if ( isThrough ) this.through();
         if(this.checkPassage(nextMapY, nextMapX)){
             if(stope) {
                 this.createCounterMounth();
@@ -62,7 +61,7 @@ export default class Pacman {
             }
             return this[v] += shift;
         }
-        if (gameMap[mapCoordY][mapCoordX] === 'X') return this.through();
+        if (gameMap[mapCoordY][mapCoordX] === 'X' && !isThrough) return this.through();
 
         if(changeCourse){
             let [changeX, changeY] = changeCourse;
@@ -70,37 +69,43 @@ export default class Pacman {
             this.direction = changeCourse;
             this.changeCourse = undefined;
         }
-        if(!stope) this.stopePacman();
+        if(!stope && !isThrough) this.stopePacman();
     }
 
 
     through(){
-        let { direction: [directionX], mapCoord:[row, col], ys, xs, maxX, throughData: { xCol, xD }, gameMap, speed } = this;
-        this.isThrough = true;
-        let xcol = xCol;
-        let tgx = xD;
+        let { isThrough, direction: [directionX], mapCoord:[row], ys, xs, speed } = this;
+        if (!isThrough) this.createTroughConfig(xs, directionX > 0);
+        let { throughData: { startPositionPaint , endTrough } } = this;
 
-        if ( xCol === undefined ) {
-            xcol = col < 2 ? maxX : 0;
-            tgx = col < 2 ? xs : -xs;
-        }
-        tgx += directionX * speed;
-        console.log(tgx, xcol);
+        this.throughData.startPositionPaint += directionX * speed;
         this.x += directionX * speed;
-        this.paintPac( xcol - (25 - tgx) , row * ys)
+        console.log(startPositionPaint , endTrough)
 
-        if(tgx === 0) {
-            let { mapCoord:[ir, ic] } = this;
-            this.throughData['xD'] = undefined;
-            this.throughData['xCol'] = undefined;
+        if (startPositionPaint === endTrough) return this.endTrough(endTrough, xs);
 
-            this.mapCoord = [ir, xcol > 0 ? gameMap[0].length : 0 ];
-            this.nextMapCoord = [ir, this.mapCoord[1] + directionX];
-            this.x = xcol > 0 ? maxX : 0;
-            return this.isThrough = false;
+        this.paintPac( startPositionPaint , row * ys)
+
+
+
+    }
+
+    createTroughConfig (xSteep, isLeft) {
+        let { maxX } = this;
+        this.throughData = {
+            startPositionPaint: isLeft ? -xSteep : maxX + xSteep,
+            endTrough: isLeft ? 0 : maxX
         }
-        this.throughData['xD'] = tgx;
-        this.throughData['xCol'] = xcol;
+        this.isThrough = true;
+    }
+
+    endTrough(endTrough) {
+        let { mapCoord: [row, col], gameMap } = this;
+        this.x = endTrough;
+        console.log(endTrough)
+        this.mapCoord = [ row, endTrough === 0 ? 0 : gameMap[0].length-1]
+        this.nextMapCoord = [ row, endTrough === 0 ? 1 : gameMap[0].length-2]
+        return this.isThrough = false;
     }
 
     setPropertyOnClick = (isDirection, x, y) => {
@@ -109,7 +114,7 @@ export default class Pacman {
             if(endChangeDirection === x || endChangeDirection === y) return;
             if(this.checkPassage(ir + y, ic + x)){
                 this.direction = [x , y];
-                this.nextMapCoord = [ir + y , ic + x]
+                this.nextMapCoord = [ir + y , ic + x];
                 this.mapCoord = [ir , ic];
                 this.endChangeDirection = x === 0 ? y : x;
             }
@@ -146,7 +151,7 @@ export default class Pacman {
 
     move() {
         let {x, y, direction: [ dx, dy], speed, xs, ys, mapCoord: [ir, ic], nextMapCoord: [nir, nic]} = this;
-        
+
         if(ic !== nic) return this.updateCoords(x , xs * nic, speed * dx, 'x');
         if(ir !== nir) return this.updateCoords(y , ys * nir, speed * dy, 'y');
     }
